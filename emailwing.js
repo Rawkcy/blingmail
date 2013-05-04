575d253d24de684e266fcf47dbaeb0e7b952ce14
Roxanne Guo <roxane.guo@gmail.com>
2013-05-04 16:33:37 -0700

    why gmail sho evil

--- emailwing.js
+++ emailwing.js
@@ -1,1 +1,4 @@
-alert("hello emailwing!");
+$(document).ready(function() {
+  alert("hello emailwing!");
+  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td>OH MY GOODNESS</td></tr></tbody></table></div>');
+});
