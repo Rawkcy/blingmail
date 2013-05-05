61f33b4bf15e47eb9e3c8f5202e3144ff193f224
Sarah Hum <sarshum@gmail.com>
2013-05-04 20:23:37 -0700

    Input form style

--- js/emailwing.js
+++ js/emailwing.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  alert("emailwing!");
-  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve">Get Approved</button></td><td><input style="width:200px;"></td></tr></tbody></table></div>');
+  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table><tbody><tr><td><button id="approve">Get Approved</button></td><td><input id="approval-emails" placeholder="Enter emails"></td></tr></tbody></table></div>');
   $('button#approve').click(function() {
     var body = $('div.LW-avf').text();
     var to = $('div.vT').text();
