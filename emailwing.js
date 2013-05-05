eb3528820e55aaee728e1f73d1f9c1b464ff525c
Roxanne Guo <roxane.guo@gmail.com>
2013-05-04 17:30:06 -0700

    push to firebase

--- emailwing.js
+++ emailwing.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-  alert("hello emailwing!");
-  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td>OH MY GOODNESS</td></tr></tbody></table></div>');
+  alert("emailwing!");
+  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button style="padding: 2px 18px; line-height: 27px; font-size: 11px; font-weight: bold; color: white; background: orange; border: none; background-image: -webkit-linear-gradient(top, #25ce23, #1fb71d); border-radius: 2px; border: 1px solid #1fb71d;">Approve</button></td></tr></tbody></table></div>');
+  var body = $('div.LW-avf').text();
+  //var to = $('textarea[name=to]').val;
+  alert(body);
+  //alert(to);
+
+  var db = new Firebase('https://blingmail.firebaseio.com/');
+  db.set(body);
 });
