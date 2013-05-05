c94510912372480568ae4231e7936dc4808b4020
Sarah Hum <sarshum@gmail.com>
2013-05-04 19:23:53 -0700

    conflict

--- emailwing.js
+++ emailwing.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function() {
-  alert("emailwing!");
-  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve"; style="padding: 2px 18px; line-height: 27px; font-size: 11px; font-weight: bold; color: white; background: orange; border: none; background-image: -webkit-linear-gradient(top, #25ce23, #1fb71d); border-radius: 2px; border: 1px solid #1fb71d;">Approve</button></td><td><input style="width:200px;"></td></tr></tbody></table></div>');
+  // alert("hello");
+  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve"; style="padding: 0px 15px; line-height: 27px; font-size: 11px; font-weight: bold; color: white; background: orange; border: none; background-image: -webkit-linear-gradient(top, #25ce23, #1fb71d); border-radius: 2px; border: 1px solid #1ba819;">Get Approved</button></td></tr></tbody></table></div>');
   var body = $('div.LW-avf').text();
   var to = $('div.vT').text();
   var id = window.location.href.split('?compose=')[1];
