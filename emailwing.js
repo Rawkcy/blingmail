1fdb28e95e62ad730283038eb71abe00c6fb4acf
Roxanne Guo <roxane.guo@gmail.com>
2013-05-04 19:20:12 -0700

    need to style input

--- emailwing.js
+++ emailwing.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   alert("emailwing!");
-  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve"; style="padding: 2px 18px; line-height: 27px; font-size: 11px; font-weight: bold; color: white; background: orange; border: none; background-image: -webkit-linear-gradient(top, #25ce23, #1fb71d); border-radius: 2px; border: 1px solid #1fb71d;">Approve</button></td></tr></tbody></table></div>');
+  $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve"; style="padding: 2px 18px; line-height: 27px; font-size: 11px; font-weight: bold; color: white; background: orange; border: none; background-image: -webkit-linear-gradient(top, #25ce23, #1fb71d); border-radius: 2px; border: 1px solid #1fb71d;">Approve</button></td><td><input style="width:200px;"></td></tr></tbody></table></div>');
   var body = $('div.LW-avf').text();
   var to = $('div.vT').text();
-  var id = window.location.href.split('?compose=')[1]);
+  var id = window.location.href.split('?compose=')[1];
   $('button#approve').click(function() {
     alert('body: ' + body);
     alert('to: ' + to);
     alert('id: ' + id);
-  });
 
-  var db = new Firebase('https://blingmail.firebaseio.com/');
-  db.set(body);
+    var db = new Firebase('https://blingmail.firebaseio.com/');
+    db.set(body);
+  });
 });
