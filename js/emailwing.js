e5bb2d9f39b63d7f5d2536e9d01feaed1e61eeaf
Roxanne Guo <roxane.guo@gmail.com>
2013-05-04 20:36:44 -0700

    add id for table

--- js/emailwing.js
+++ js/emailwing.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // activate js code on "COMPOSE"
   $('div.T-I.J-J5-Ji.T-I-KE.L3').click(function() {
     //alert("emailwing activate!");
-    $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table style="width=100%"><tbody><tr><td><button id="approve">Get Approved</button></td><td><input placeholder="Enter emails" id="approval-emails"></td></tr></tbody></table></div>');
+    $('div.aDh').after('<div style="background-color: whiteSmoke; border: 1px solid #CFCFCF; border-width: 0 1px 1px 1px; margin: 0 -1px; overflow-y: hidden;"><table id="approval-fields"><tbody><tr><td><button id="approve">Get Approved</button></td><td><input placeholder="Enter emails" id="approval-emails"></td></tr></tbody></table></div>');
     $('button#approve').click(function() {
       var body = $('div.LW-avf').text();
       var to = $('div.vT').text();
